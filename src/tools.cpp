0274cd8c7f4ec5d4790fcfa509cc608ffb3a9337
jurgis <jurgis@trafi.com>
2018-10-17 07:41:29 +0800

    Run from data file

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,14 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    auto n = estimations.size();
+    VectorXd sum = VectorXd::Zero(4);
+    for (size_t i = 0; i < n; i++) {
+        VectorXd diff = estimations.at(i) - ground_truth.at(i);
+        sum = sum + diff.cwiseProduct(diff);
+    }
+
+    sum = sum / n;
+    sum = sum.cwiseSqrt();
+    return sum;
 }
